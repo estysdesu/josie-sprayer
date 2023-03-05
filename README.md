2296e0f6d7fc9eedd25c18d44f2809a9cb6dfee8
Tyler Estes <tyler.estes@estysdesu.com>
2023-03-05 12:28:06 -0500

    stepper standby code & some debugging additions that aren't quite working yet

---
+++ README.md
@@ -0,0 +1,10 @@
+# Description
+My dog was tearing up the back of my couch by laying on the top of the cushion all day while my wife & I are away at work. Needed a sense & deter solution...
+
+Ultrasonic sensor with stepper motor pulling the handle of a spray bottle upon fixed distance detection.
+
+# Hardware
+* Sharp 2Y0A02 infrared sensor
+* StepperOnline 17HE08-1004S stepper motor
+* Adafruit TB6612 motor driver
+* Random 12V 2A power supply
--- src/PRJ23002_Josie_Sprayer.cpp
+++ src/PRJ23002_Josie_Sprayer.cpp
@@ -1,9 +1,11 @@
+//#include "avr8-stub.h"
+//#include "app_api.h"
 #include <Arduino.h>
 #include <RunningMedian.h>
 #include <Stepper.h>
 
 // SENSOR
-const float TARGET_RANGE = 100.0; // sensor target range [cm]
+const float TARGET_RANGE = 61.0; // sensor target range [cm] -- 61cm ~= 24in
 const int N_SAMPLES = 50; // number of samples to take
 const float N_SAMPLES_AVG = 40; // number samples of median samples to average
 RunningMedian voltageSamples = RunningMedian(N_SAMPLES);
@@ -12,6 +14,7 @@ RunningMedian distSamples = RunningMedian(N_SAMPLES);
 // STEPPER
 const int STEPS=200; // motor steps (360/<step angle>)
 Stepper stepper(STEPS, 4, 5, 6, 7);
+
 
 float DAC(unsigned int analogueValue) {
   return map(analogueValue, 0, 1023, 0, 5000) / 1000.0;
@@ -22,13 +25,19 @@ float sensorConvertDist(float voltageValue) {
 }
 
 void setup() {
+  // DEBUGGING
+  //debug_init(); // init GDB stub 
+  Serial.begin(9600);   // start serial monitor at 9600 baud rate
 
+  // VISUAL FEEDBACK -- BUILTIN LED INITIALIZE LOW
   pinMode(LED_BUILTIN, OUTPUT);   // initialize digital pin LED_BUILTIN as an output.
   digitalWrite(LED_BUILTIN, LOW); // initialize LED off
 
-  stepper.setSpeed(60);
-
-  Serial.begin(9600);   // start serial monitor at 9600 baud rate
+  // STEPPER -- SET DEFAULT SPEED
+  stepper.setSpeed(480); // RPM
+  // STEPPER -- SET STANDBY MODE INITIALLY
+  pinMode(PIN3, INPUT);
+  digitalWrite(PIN3, HIGH);
 
 }
  
@@ -50,11 +59,15 @@ void loop() {
 
   if (voltage < 0.5 || voltage > 2.8) {
     dist = NAN; // sensor is not stable outside of this voltage range
+    digitalWrite(LED_BUILTIN, LOW); // ensure LED off
+    digitalWrite(PIN3, HIGH); // ensure motor driver in standby
   } else if (dist < TARGET_RANGE) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    stepper.step(STEPS);
+    digitalWrite(LED_BUILTIN, HIGH); // LED on
+    digitalWrite(PIN3, LOW); // take the motor driver out of standby
+    stepper.step(STEPS*5); // rotate motor 5 times
   } else {
-    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(LED_BUILTIN, LOW); // ensure LED off
+    digitalWrite(PIN3, HIGH); // ensure motor driver in standby
   }
 
   Serial.print("v_out:");
